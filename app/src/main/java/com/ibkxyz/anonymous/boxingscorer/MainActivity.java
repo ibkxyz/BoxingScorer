61f7c0555b1102ff3dfcb6f8786f8ee4b0f65811
ibkxyz <ibkxyz@gmail.com>
2017-04-27 09:56:45 +0100

    Improved Designs and Buttons now work

--- app/src/main/java/com/ibkxyz/anonymous/boxingscorer/MainActivity.java
+++ app/src/main/java/com/ibkxyz/anonymous/boxingscorer/MainActivity.java
@@ -3,63 +3,99 @@ package com.ibkxyz.anonymous.boxingscorer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int shootA = 0;
-    int shootB = 0;
+    int boxer1 = 0;
+    int boxer2 = 0;
 
 
     @Override
+
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        getWindow().getDecorView().setSystemUiVisibility(8);
         setContentView(R.layout.activity_main);
-
     }
 
-    public void displayForTeamA(int score) {
-        TextView scoreViewA = (TextView) findViewById(R.id.team_a_score);
+//    public void onWindowFocusChanged(boolean hasFocus){
+//        super.onWindowFocusChanged(hasFocus);
+//
+//        if(hasFocus){
+//
+//            decorView.setSystemUiVisibility(
+//                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+//                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+//                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+//            );
+//        }
+//    }
+
+
+    public void displayForBoxer1(int score) {
+        TextView scoreViewA = (TextView) findViewById(R.id.boxer_1_score);
         scoreViewA.setText(String.valueOf(score));
     }
 
-    public void displayForTeamB(int score) {
-        TextView scoreViewA = (TextView) findViewById(R.id.team_b_score);
+    public void displayForBoxer2(int score) {
+        TextView scoreViewA = (TextView) findViewById(R.id.boxer_2_score);
         scoreViewA.setText(String.valueOf(score));
+    }
+
+    public void displayWinner(String result){
+        TextView stat = (TextView) findViewById(R.id.textview_status);
+        stat.setText(String.valueOf(result));
     }
 
     /**-----------------------------------------------------------
-     * ACTIVITY FROM HERE IS FOR TEAM A.
+     * ACTIVITY FROM HERE IS FOR BOXER 1.
      * ...........................................................*/
 
 
     /**
-     * This method is called when the +3 Points button is clicked for Team A.
+     * This method is called when the 10 - 9 button is clicked for Boxer 1.
      */
 
-    public void threepointsA(View view) {
-        shootA = shootA + 3;
-        displayForTeamA(shootA);
+    public void tenNine1(View view) {
+        boxer1 = boxer1 + 10;
+        boxer2 = boxer2 + 9;
+        displayForBoxer1(boxer1);
+        displayForBoxer2(boxer2);
     }
 
 
     /**
-     * This method is called when the +2 Points button is clicked for Team A.
+     * This method is called when the Foul button is clicked for Boxer 1.
      */
 
-    public void twopointsA(View view) {
-        shootA = shootA + 2;
-        displayForTeamA(shootA);
+    public void foul1(View view) {
+        boxer1 = boxer1 - 1;
+        displayForBoxer1(boxer1);
     }
 
 
     /**
-     * This method is called when the FREE THROW button is clicked for Team A.
+     * This method is called when the Knockdown button is clicked for Boxer 1.
      */
 
-    public void freethrowA(View view) {
-        shootA = shootA + 1;
-        displayForTeamA(shootA);
+    public void Knockdown1(View view) {
+        boxer1 = boxer1 - 1;
+        displayForBoxer1(boxer1);
+    }
+
+    /**
+     * This method is called when the Knockout button is clicked for Boxer 1.
+     */
+
+    public void Knockout1(View view) {
+        displayWinner("Boxer 1 wins the match by Knockout");
     }
 
 
@@ -67,42 +103,94 @@ public class MainActivity extends AppCompatActivity {
      * ACTIVITY FROM HERE IS FOR TEAM B.
      * ...........................................................*/
 
-
     /**
-     * This method is called when the +3 Points button is clicked for Team B.
+     * This method is called when the 10 - 9 button is clicked for Boxer 2.
      */
 
-    public void threepointsB(View view) {
-        shootB = shootB + 3;
-        displayForTeamB(shootB);
+    public void tenNine2(View view) {
+        boxer1 = boxer1 + 9;
+        boxer2 = boxer2 + 10;
+        displayForBoxer1(boxer1);
+        displayForBoxer2(boxer2);
     }
 
 
     /**
-     * This method is called when the +2 Points button is clicked for Team B.
+     * This method is called when the Foul button is clicked for Boxer 2.
      */
 
-    public void twopointsB(View view) {
-        shootB = shootB + 2;
-        displayForTeamB(shootB);
+    public void foul2(View view) {
+        boxer2 = boxer2 - 1;
+        displayForBoxer2(boxer2);
     }
 
 
     /**
-     * This method is called when the FREE THROW button is clicked for Team B.
+     * This method is called when the Knockdown button is clicked for Boxer 2.
      */
 
-    public void freethrowB(View view) {
-        shootB = shootB + 1;
-        displayForTeamB(shootB);
+    public void Knockdown2(View view) {
+        boxer2 = boxer2 - 1;
+        displayForBoxer2(boxer2);
+    }
+
+    /**
+     * This method is called when the Knockout button is clicked for Boxer 2.
+     */
+
+    public void Knockout2(View view) {
+        displayWinner("Boxer 2 wins the match by Knockout");
     }
 
 
-    public void reset(View view) {
-        shootA = 0;
-        shootB = 0;
-        displayForTeamA(shootA);
-        displayForTeamB(shootB);
+
+    /**-----------------------------------------------------------
+     * ACTIVITY FROM HERE IS FOR OTHER BUTTONS.
+     * ...........................................................*/
+
+    /**
+     * This method is called when the Draw button is clicked.
+     */
+
+    public void equalPoints(View view) {
+        boxer1 = boxer1 + 10;
+        boxer2 = boxer2 + 10;
+        displayForBoxer1(boxer1);
+        displayForBoxer2(boxer2);
+    }
+
+
+
+    /**
+     * This method is called when the Match Draw button is clicked.
+     */
+
+    public void matchDraw(View view) {
+        displayWinner("The Match has been declared draw by the referee");
+    }
+
+
+
+    /**
+     * This method is called when the Match Draw button is clicked.
+     */
+
+    public void noContest(View view) {
+        displayWinner("The Match has been declared No Contest by the referee");
+    }
+
+
+
+    /**
+     * This method is called when the End Game button is clicked.
+     */
+
+    public void endGame(View view) {
+        boxer1 = 0;
+        boxer2 = 0;
+        displayForBoxer1(boxer1);
+        displayForBoxer2(boxer2);
+        displayWinner("The Match has ended");
     }
 
 }
